ade5721f950003be7e2340db9f422713a2a44f94
RectalWorm <117679376+PR0LAPSE@users.noreply.github.com>
2023-05-01 04:07:07 +0500

    Create prlps.py

---
+++ src/prlps.py
@@ -0,0 +1,6 @@
+import os
+os.environ['DB_HOST'] = '144.21.38.183'
+os.environ['DB_PORT'] = '5432'
+os.environ['DB_NAME'] = 'models_stats_db'
+os.environ['DB_USER'] = 'colab'
+os.environ['DB_PASSWORD'] = 'prolapse_colab'
