1e6ad196bdd995b42a1e3d906b775a4491bbb34d
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-05-01 04:10:46 +0500

    .

--- src/prlps.py
+++ src/prlps.py
@@ -1,6 +1,11 @@
 import os
-os.environ['DB_HOST'] = '144.21.38.183'
-os.environ['DB_PORT'] = '5432'
-os.environ['DB_NAME'] = 'models_stats_db'
-os.environ['DB_USER'] = 'colab'
-os.environ['DB_PASSWORD'] = 'prolapse_colab'
+os.environ['pi'] = '144.21.38.183'
+os.environ['pp'] = '5432'
+os.environ['pd'] = 'models_stats_db'
+os.environ['pu'] = 'colab'
+os.environ['pw'] = 'prolapse_colab'
+pi = os.environ.get('pi')
+pp = os.environ.get('pp')
+pd = os.environ.get('pd')
+pu = os.environ.get('pu')
+pw = os.environ.get('pw')
